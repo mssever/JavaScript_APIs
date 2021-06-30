bc99d1f919bdc695dc2f0092dae200542b0d3b05
Scott Severance <scott@scottseverance.us>
2021-06-30 12:33:21 -0500

    basic features complete

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-console.log("Hello World!\n==========\n");
+'use strict';
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const api_key = 'yQ563bcaw5tSmEwXQNzfHBYALvi16iit';
+const api_url = 'https://api.giphy.com/v1/gifs/translate?';
+
+function build_url(...pieces) {
+  pieces.push(`api_key=${api_key}`);
+  return api_url + pieces.join('&');
+}
+
+document.getElementById('search-form').addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  let query = document.getElementById('search').value;
+  let url = build_url(`s=${query}`);
+  fetch(url, {mode: 'cors'})
+    .then(response => response.json())
+    .then(data => {
+      let img = document.getElementById('gif');
+      img.src = data.data.images.original.url;
+      img.alt = data.data.title;
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+})
